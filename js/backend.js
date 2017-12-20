42874edb558ece3759f5beab43c511f18fece431
MishaGl <mishaglansev@gmail.com>
2017-12-20 16:52:15 +0300

    load save

--- js/backend.js
+++ js/backend.js
@@ -1,14 +1,45 @@
 'use strict';
 (function () {
+  var SERVER_URL = 'https://1510.dump.academy/keksobooking/data';
+
+  var setup = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError(xhr.response);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000; // 10s
+
+    return xhr;
+  };
+
   function load(onLoad, onError) {
-    var xhr = setup(onSuccess, onError);
+    var xhr = setup(onLoad, onError);
     xhr.open('GET', SERVER_URL + '/data');
     xhr.send();
   }
 
   function save(data, onLoad, onError) {
-    var xhr = setup(onSuccess, onError);
+    var xhr = setup(onLoad, onError);
     xhr.open('POST', SERVER_URL);
     xhr.send(data);
   }
+
+  window.beckend = {
+    load: load,
+    save: save
+  };
+
 })();

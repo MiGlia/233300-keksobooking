b35c395ddc4121b0ea4e856fa3716f45c7566fef
MishaGl <mishaglansev@gmail.com>
2018-03-13 20:48:06 +0300

    clear

--- js/synchronize-fields.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-(function () {
-  //  универсальной функцией для синхронизации полей
-  window.synchronizeFields = function (firstField, secondField, firstArray, secondArray, functSyncValues) {
-    var index = firstArray.indexOf(firstField.value);
-    functSyncValues(secondField, secondArray[index]);
-  };
-}());
